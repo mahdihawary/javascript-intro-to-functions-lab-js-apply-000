5ffc06325118d8e6258e16fef56e6d80e9884a3c
Mahdi Hawary <mahdihawary@gmail.com>
2019-10-03 04:44:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function whisper(String){
   return String.toLowerCase()
 }
 function logShout(String){
-  console.log(String.toLowerCase())
+  console.log(String.toUpperCase())
 }
 function logWhisper(String){
-  
+  console.log(String.toLowerCase())
 }
 function SayHiToGrandma(String){
   
